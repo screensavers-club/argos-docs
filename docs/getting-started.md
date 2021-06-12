3b8ff3995118551a1f1c10eb35679ee9efb51eed
Siah <i@siah.sg>
2021-06-12 21:23:59 +0800

    feat: initial content population

---
+++ docs/getting-started.md
@@ -0,0 +1,14 @@
+---
+sidebar_label: "Getting Started"
+sidebar_position: 1
+---
+
+# Getting Started
+
+Argos is an integrated hardware-software project xxx xxx xxx.
+
+## Motivation
+
+Why we created Argos.
+
+## Problems
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,102 +1,93 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+	title: "Argos",
+	tagline: "Realtime distributed performance gear",
+	url: "https://argos.studio",
+	baseUrl: "/",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "img/favicon.ico",
+	organizationName: "screensavers-club", // Usually your GitHub org/user name.
+	projectName: "argos", // Usually your repo name.
+	themeConfig: {
+		navbar: {
+			title: "Argos",
+			// logo: {
+			// 	alt: "My Site Logo",
+			// 	src: "img/logo.svg",
+			// },
+			items: [
+				{
+					type: "doc",
+					docId: "getting-started",
+					position: "left",
+					label: "Getting Started",
+				},
+				{ to: "/blog", label: "Dev Log", position: "left" },
+				{
+					href: "https://github.com/screensavers-club/argos-docs",
+					label: "GitHub",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [
+				{
+					title: "Docs",
+					items: [
+						{
+							label: "Getting Started",
+							to: "/docs/getting-started",
+						},
+					],
+				},
+				{
+					title: "Community",
+					items: [
+						{
+							label: "Github Repo",
+							href: "https://github.com/screensavers-club/argos-docs",
+						},
+					],
+				},
+				{
+					title: "More",
+					items: [
+						{
+							label: "Dev Log",
+							to: "/blog",
+						},
+						{
+							label: "GitHub",
+							href: "https://github.com/facebook/docusaurus",
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} Argos`,
+		},
+	},
+	presets: [
+		[
+			"@docusaurus/preset-classic",
+			{
+				docs: {
+					sidebarPath: require.resolve("./sidebars.js"),
+					// Please change this to your repo.
+					editUrl:
+						"https://github.com/facebook/docusaurus/edit/master/website/",
+				},
+				blog: {
+					// Please change this to your repo.
+					editUrl:
+						"https://github.com/facebook/docusaurus/edit/master/website/devlog",
+				},
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			},
+		],
+	],
 };
