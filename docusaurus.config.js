c7fff55dd7ed805022c892c1cff92aacb80b50c9
Siah Tiong Hong <siah@goodwork.sg>
2021-11-25 18:17:41 +0800

    chore: image optimisation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,93 +1,95 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-	title: "Argos",
-	tagline: "Realtime distributed performance gear",
-	url: "https://argos.studio",
-	baseUrl: "/",
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
-	favicon: "img/favicon.ico",
-	organizationName: "screensavers-club", // Usually your GitHub org/user name.
-	projectName: "argos", // Usually your repo name.
-	themeConfig: {
-		navbar: {
-			title: "Argos",
-			// logo: {
-			// 	alt: "My Site Logo",
-			// 	src: "img/logo.svg",
-			// },
-			items: [
-				{
-					type: "doc",
-					docId: "getting-started",
-					position: "left",
-					label: "Getting Started",
-				},
-				{ to: "/blog", label: "Dev Log", position: "left" },
-				{
-					href: "https://github.com/screensavers-club/argos-docs",
-					label: "GitHub",
-					position: "right",
-				},
-			],
-		},
-		footer: {
-			style: "dark",
-			links: [
-				{
-					title: "Docs",
-					items: [
-						{
-							label: "Getting Started",
-							to: "/docs/getting-started",
-						},
-					],
-				},
-				{
-					title: "Community",
-					items: [
-						{
-							label: "Github Repo",
-							href: "https://github.com/screensavers-club/argos-docs",
-						},
-					],
-				},
-				{
-					title: "More",
-					items: [
-						{
-							label: "Dev Log",
-							to: "/blog",
-						},
-						{
-							label: "GitHub",
-							href: "https://github.com/facebook/docusaurus",
-						},
-					],
-				},
-			],
-			copyright: `Copyright © ${new Date().getFullYear()} Argos`,
-		},
-	},
-	presets: [
-		[
-			"@docusaurus/preset-classic",
-			{
-				docs: {
-					sidebarPath: require.resolve("./sidebars.js"),
-					// Please change this to your repo.
-					editUrl:
-						"https://github.com/facebook/docusaurus/edit/master/website/",
-				},
-				blog: {
-					// Please change this to your repo.
-					editUrl:
-						"https://github.com/facebook/docusaurus/edit/master/website/devlog",
-				},
-				theme: {
-					customCss: require.resolve("./src/css/custom.css"),
-				},
-			},
-		],
-	],
+  title: "XIMI",
+  tagline: "Realtime distributed performance gear",
+  url: "https://argos.studio",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "screensavers-club", // Usually your GitHub org/user name.
+  projectName: "argos-docs", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "XIMI",
+      items: [
+        {
+          type: "doc",
+          docId: "user-manual/artists/switching-on",
+          position: "left",
+          label: "For Performers",
+        },
+        {
+          type: "doc",
+          docId: "user-manual/stage-managers/ximi-stage",
+          position: "left",
+          label: "For Stage Managers",
+        },
+
+        {
+          href: "https://github.com/screensavers-club/argos-docs",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    // footer: {
+    //   style: "dark",
+    //   links: [
+    //     {
+    //       title: "Docs",
+    //       items: [
+    //         {
+    //           label: "Getting Started",
+    //           to: "/docs/getting-started",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: "Community",
+    //       items: [
+    //         {
+    //           label: "Github Repo",
+    //           href: "https://github.com/screensavers-club/argos-docs",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: "More",
+    //       items: [
+    //         {
+    //           label: "Dev Log",
+    //           to: "/blog",
+    //         },
+    //         {
+    //           label: "GitHub",
+    //           href: "https://github.com/facebook/docusaurus",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    //   copyright: `Copyright © ${new Date().getFullYear()} Argos`,
+    // },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/devlog",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
 };
